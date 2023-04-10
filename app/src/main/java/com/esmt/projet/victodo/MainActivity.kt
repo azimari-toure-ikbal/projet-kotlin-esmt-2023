cdcc7171bd61d7cb1c9cf18be7daa460358897ae
Ikbal AZIMARI TOURE <azimari.toure.etu@esmt.sn>
2023-04-10 20:21:28 +0000

    onboarding screen almost done 90%

--- app/src/main/java/com/esmt/projet/victodo/MainActivity.kt
+++ app/src/main/java/com/esmt/projet/victodo/MainActivity.kt
@@ -3,13 +3,17 @@ package com.esmt.projet.victodo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
@@ -19,18 +23,23 @@ import com.esmt.projet.victodo.core.presentation.util.Screen
 import com.esmt.projet.victodo.feature_onboarding.presentation.welcome_screen.SplashViewModel
 import com.esmt.projet.victodo.feature_onboarding.presentation.welcome_screen.WelcomeScreen
 import com.esmt.projet.victodo.ui.theme.VictoDoTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-//    @Inject
-//    lateinit var splashViewModel: SplashViewModel
+    @Inject
+    lateinit var splashViewModel: SplashViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        installSplashScreen().setKeepOnScreenCondition {
-//            !splashViewModel.isLoading.value
-//        }
+        installSplashScreen().setKeepOnScreenCondition {
+            !splashViewModel.isLoading.value
+        }
 
         setContent {
             VictoDoTheme {
@@ -39,18 +48,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-//                    val screen by splashViewModel.startDestination
+                    val screen by splashViewModel.startDestination
                     val navController = rememberNavController()
 
                     NavHost(
                         navController = navController,
-                        startDestination = Screen.OnBoardingScreen.route
+                        startDestination = screen
                     ) {
                         composable(route = Screen.OnBoardingScreen.route) {
                             WelcomeScreen(navController = navController)
                         }
                         composable(route = Screen.HomeScreen.route) {
-
+                            Greeting("Android")
                         }
                     }
                 }
@@ -61,7 +70,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .fillMaxSize(),
+        textAlign = TextAlign.Center,
+        fontSize = MaterialTheme.typography.h1.fontSize,
+        fontWeight = FontWeight.Bold,
+    )
 }
 
 @Preview(showBackground = true)
