6b9697fc3b877cd87ffa21aa5b4d0016bce4009b
Ikbal AZIMARI TOURE <azimari.toure.etu@esmt.sn>
2023-04-17 10:09:08 +0000

    Update AddEditListViewModel.kt

--- app/src/main/java/com/esmt/projet/victodo/feature_list/presentation/add_edit_screen/AddEditListViewModel.kt
+++ app/src/main/java/com/esmt/projet/victodo/feature_list/presentation/add_edit_screen/AddEditListViewModel.kt
@@ -3,19 +3,23 @@ package com.esmt.projet.victodo.feature_list.presentation.add_edit_screen
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.toArgb
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.esmt.projet.victodo.exception.list.InvalidListException
 import com.esmt.projet.victodo.feature_list.domain.model.TaskList
 import com.esmt.projet.victodo.feature_list.domain.use_case.TaskListUseCases
+import com.esmt.projet.victodo.feature_list.presentation.add_edit_screen.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class AddEditViewModel @Inject constructor(
-    private val listUseCases: TaskListUseCases
+    private val listUseCases: TaskListUseCases,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     private val _listTitle = mutableStateOf(AddEditListsState(
@@ -29,6 +33,20 @@ class AddEditViewModel @Inject constructor(
     private val _listIcon = mutableStateOf<String>(TaskList.listIcons.random().toString())
     val listIcon : State<String> = _listIcon
 
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+
+    private var currentListId : Long? = null
+
+    init {
+        savedStateHandle.get<Long>("listId")?.let { listId ->
+            if (listId != -1L) {
+                viewModelScope.launch {
+                    
+                }
+            }
+        }
+    }
+
     fun onEvent(event: AddEditListsEvent) {
         when (event) {
             is AddEditListsEvent.EnteredTitle -> {
@@ -39,7 +57,7 @@ class AddEditViewModel @Inject constructor(
                     try {
                         listUseCases.addTaskListUseCase(
                             TaskList(
-                                id = generateUUID(),
+                                id = currentListId ?: generateUUID(),
                                 title = event.taskList.title,
                                 color = event.taskList.color,
                                 isPinned = false,
@@ -47,8 +65,9 @@ class AddEditViewModel @Inject constructor(
                                 isDefault = false
                             )
                         )
+                        _eventFlow.emit(UiEvent.NavigateBackWithResult(Result.OK.ordinal))
                     } catch (e: InvalidListException) {
-
+                        _eventFlow.emit(UiEvent.ShowInvalidInputMessage(e.message ?: "Couldn't create list"))
                     }
                 }
             }
@@ -62,6 +81,15 @@ class AddEditViewModel @Inject constructor(
     }
 }
 
+sealed class UiEvent {
+    data class ShowInvalidInputMessage(val message: String) : UiEvent()
+    data class NavigateBackWithResult(val result: Int) : UiEvent()
+}
+
 private fun generateUUID(): Long {
     return UUID.randomUUID().mostSignificantBits and Long.MAX_VALUE
+}
+
+private enum class Result {
+    OK, CANCELED
 }
