7a43f3bdd21ad899e800dba8195eff7521527549
Ikbal AZIMARI TOURE <azimari.toure.etu@esmt.sn>
2023-05-02 09:26:08 +0000

    Update HomeScreen.kt

--- app/src/main/java/com/esmt/projet/victodo/core/presentation/HomeScreen.kt
+++ app/src/main/java/com/esmt/projet/victodo/core/presentation/HomeScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -102,9 +103,14 @@ fun HomeScreen(
             for (taskList in state.listOfPinnedList) {
                 Box(
                     modifier = Modifier
-                        .padding(start = 8.dp, end = 8.dp, bottom = 12.dp)
-                        .height(150.dp)
-                        .width(150.dp)
+                        .padding(start = 28.dp, end = 8.dp, bottom = 12.dp)
+                        .height(120.dp)
+                        .width(120.dp)
+                        .shadow(
+                            elevation = 8.dp,
+                            shape = RoundedCornerShape(10.dp),
+                            spotColor = Color(0xFF006EE9)
+                        )
                         .clip(RoundedCornerShape(12.dp))
                         .clickable {
                             navController.navigate(
@@ -119,7 +125,7 @@ fun HomeScreen(
                         horizontalAlignment = Alignment.CenterHorizontally,
                         modifier = Modifier
                             .fillMaxSize()
-                            .background(Color(0xFFAAE6FC))
+                            .background(Color(0xFFEEF5FD))
                     ) {
                         Row(
                             horizontalArrangement = Arrangement.End,
@@ -137,6 +143,7 @@ fun HomeScreen(
                             ) {
                                 Text(
                                     text = taskList.tasks.size.toString(),
+                                    color = Color(0xFF006EE9),
                                     fontSize = 10.sp,
                                 )
                             }
@@ -147,7 +154,7 @@ fun HomeScreen(
                             overflow = TextOverflow.Ellipsis,
                             fontSize = 16.sp,
                             fontWeight = FontWeight.SemiBold,
-                            color = Color.White,
+                            color = Color(0xFF006EE9),
                             modifier = Modifier
                                 .padding(top = 30.dp, start = 2.dp, end = 2.dp)
                         )
@@ -276,71 +283,52 @@ fun HomeScreen(
                 .fillMaxWidth()
                 .padding(bottom = 12.dp)
         ) {
-            Box(
-                modifier = Modifier
-                    .height(35.dp)
-                    .width(90.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color(0xFF006EE9))
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(start = 8.dp)
-                        .clickable {
-                            navController.navigate(Screen.AddEditTaskScreen.route)
-                        }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = null,
-                        tint = Color.White,
-                    )
-                    Text(
-                        text = "New Task",
-                        fontSize = 12.sp,
-                        color = Color.White,
-                    )
-                }
-            }
 
-            Box(
-                modifier = Modifier
-                    .height(35.dp)
-                    .width(90.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color(0xFFEEF5FD))
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(start = 8.dp)
-                        .clickable {
-                            navController.navigate(Screen.AddEditListScreen.route)
-                        }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = null,
-                        tint = Color(0xFF006EE9),
-                    )
-                    Text(
-                        text = "New List",
-                        fontSize = 12.sp,
-                        color = Color(0xFF006EE9),
-                    )
-                }
-            }
+            BottomButtons(title = "New Task", navController = navController, route = Screen.AddEditTaskScreen.route)
+            BottomButtons(title = "New List", navController = navController, route = Screen.AddEditListScreen.route)
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun Preview() {
-    HomeScreen(navController = rememberNavController())
+fun BottomButtons(
+    title: String,
+    navController: NavController,
+    route: String
+) {
+    Box(
+        modifier = Modifier
+            .height(35.dp)
+            .width(90.dp)
+            .shadow(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .clip(RoundedCornerShape(10.dp))
+            .background(if (title == "New Task") Color(0xFF006EE9) else Color(0xFFEEF5FD))
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 8.dp)
+                .clickable {
+                    navController.navigate(route)
+                }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null,
+                tint = if (title == "New Task") Color.White else Color(0xFF006EE9),
+            )
+            Text(
+                text = title,
+                fontSize = 12.sp,
+                color = if (title == "New Task") Color.White else Color(0xFF006EE9),
+            )
+        }
+    }
+
 }
 
 fun confirmDeleteList(context: Context, taskList: TaskListWithTasksAndTagsSubTasks, viewModel: HomeScreenViewModel){
